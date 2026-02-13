cf20e239dd8713fe7ac5979198b2d4b7c5aa6766
opussf <github@zz9-za.com>
2026-02-13 13:44:45 -0800

    1.0.2

--- wowcron.lua
+++ wowcron.lua
@@ -51,6 +51,7 @@ wowCron.macros = {  -- keep a 1 to 1 mapping for macro to event.
 	["@xp"]       = { ["event"] = "PLAYER_XP_UPDATE" },
 	["@ilvl"]     = { ["event"] = "PLAYER_AVG_ITEM_LEVEL_UPDATE" },
 	["@achv"]     = { ["event"] = "ACHIEVEMENT_EARNED" },
+	["@endeavor"] = { ["event"] = "NEIGHBORHOOD_INITIATIVE_UPDATED" },
 }
 wowCron.chatChannels = {
 	["/s"]    = "SAY",
